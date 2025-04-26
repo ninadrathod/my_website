3bc26b9d76cb2d02ef9ea581c95bbfd63de7757c
ninadrathod <ninadrathod267@gmail.com>
2025-04-27 00:11:19 +0530

    using script.js to fetch API data and adding it to header in index.html

---
+++ frontend/script.js
@@ -0,0 +1,56 @@
+async function fetchAndDisplayName(property, displayPropertyId) {
+  try {
+    const apiUrl = `http://localhost:3001/api/metadata/${property}`;
+    const response = await fetch(apiUrl);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const data = await response.json();
+    document.getElementById(displayPropertyId).textContent = data.data;
+  } catch (error) {
+    console.error(`Error fetching ${property}`, error);
+    document.getElementById(displayPropertyId).textContent = 'Failed to load name.';
+  }
+}
+
+async function fetchAndReturnLink(property, linkId) {
+  try {
+    const apiUrl = `http://localhost:3001/api/metadata/${property}`;
+    const response = await fetch(apiUrl);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const data = await response.json();
+    const linkElement = document.getElementById(linkId);
+    if (linkElement) 
+    {
+      if (property === 'email_id') {
+        linkElement.href = `mailto:${data.data}`;
+      } else {
+        linkElement.href = data.data;
+      }
+    } 
+    else 
+    {
+      console.error(`Link element with ID '${linkId}' not found.`);
+    }
+  } catch (error) {
+    console.error(`Error fetching ${property}:`, error);
+    const linkElement = document.getElementById(linkId);
+    if (linkElement) {
+      linkElement.href = '#'; // Set a default or error link
+    }
+  }
+}
+
+window.onload = () => {
+  fetchAndDisplayName('name', 'name-display');
+  fetchAndDisplayName('bio', 'bio-display');
+  fetchAndDisplayName('summary', 'summary-display');
+  fetchAndDisplayName('email_id', 'email_id-display');
+  fetchAndDisplayName('linkedin', 'linkedin-display');
+  fetchAndDisplayName('github', 'github-display');
+  fetchAndReturnLink('email_id', 'email_id-link');
+  fetchAndReturnLink('linkedin', 'linkedin-link');
+  fetchAndReturnLink('github', 'github-link');
+};
